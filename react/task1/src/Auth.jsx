0befbe466fe8620814b791bb5b49c62ec470b0fc
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-13 12:18:44 +0300

    t1

--- react/task1/src/Auth.jsx
+++ react/task1/src/Auth.jsx
@@ -12,45 +12,90 @@ import Spinner from './Spinner.jsx';
 
 class Auth extends React.Component {
   state = {
-    isLoggedIn: true,
+    isLoggedIn: false,
     isLogging: false,
   };
 
   onLogin = () => {
+    console.log('Step #1');
+
     this.setState({
       isLogging: true,
     });
 
+    console.log('set state called!!');
+
     setTimeout(() => {
       this.setState({
-        isLoggedIn: false,
+        isLoggedIn: true,
         isLogging: false,
       });
     }, 2000);
+    console.log('Last Step');
   };
 
   onLogout = () => {
     this.setState({
-      isLoggedIn: true,
+      isLoggedIn: false,
     });
   };
-  // available = () => {
 
+  // render() {
+  //   let button;
+  //   if (this.state.isLoggedIn) {
+  //     button = <Login onLogin={this.handleLogin} />;
+  //   } else {
+  //     button = <Logout onLogout={this.handleLogout} />;
+  //   }
+  //   return <div>{this.state.isSpinner ? <Spinner size={40} /> : button}</div>;
   // }
 
+  // render() {
+  //   // if (this.state.isLogging) {
+  //   //   return <Spinner size={20} />;
+  //   // }
+  //   return this.state.isLoggedIn ? (
+  //     this.state.isLogging ? (
+  //       <Spinner size={20} />
+  //     ) : (
+  //       <Login onLogin={this.onLogin} />
+  //     )
+  //   ) : (
+  //     <Logout onLogout={this.onLogout} />
+  //   );
+  // }
+
+  // render() {
+  //   if (!this.state.isLoggedIn) return <Login onLogin={this.onLogin} />;
+
+  //   if (this.state.isLogging) return <Logout onLogout={this.onLogout} />;
+
+  //   return <Spinner size={20} />;
+  // }
+
+  // render() {
+  //   if (!this.state.isLogging && !this.state.isLoggedIn) {
+  //     return <Login onLogin={this.onLogin} />;
+  //   }
+  //   if (this.state.isLogging) {
+  //     return <Spinner size={32} />;
+  //   }
+  //   if (this.state.isLoggedIn && !this.state.isLogging) {
+  //     return <Logout onLogout={this.onLogout} />;
+  //   }
+  //   return null;
+  // }
   render() {
-    // if (this.state.isLogging) {
-    //   return <Spinner size={20} />;
-    // }
-    return this.state.isLoggedIn ? (
-      this.state.isLogging ? (
-        <Spinner size={20} />
-      ) : (
-        <Login onLogin={this.onLogin} />
-      )
-    ) : (
-      <Logout onLogout={this.onLogout} />
-    );
+    debugger;
+    const { isLogging, isLoggedIn } = this.state;
+
+    if (isLogging) {
+      return <Spinner size={50} />;
+    }
+    if (isLoggedIn) {
+      return <Logout onLogout={this.onLogout} />;
+    }
+    return <Login onLogin={this.onLogin} />;
   }
 }
 
