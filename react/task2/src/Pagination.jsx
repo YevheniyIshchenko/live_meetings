d8160b961a3314e9bee3cc2fbd581ec08d66fcbe
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-20 13:05:13 +0300

    task2

--- react/task2/src/Pagination.jsx
+++ react/task2/src/Pagination.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const Pagination = ({ goPrev, goNext, currentPage, usersLenght }) => {
   // input : currentPage, itemsPerPage, UsersCounts
   // output ; boolean
-  const isShowFirstPageBtn = currentPage > 1;
-  const isShowLastPageBtn = currentPage * 3 < usersLenght;
+  const isShowFirstPageBtn = currentPage === 1;
+  const isShowLastPageBtn = currentPage * 3 >= usersLenght;
   return (
     <div className="pagination">
-      <button className="btn" onClick={goPrev}>
-        {isShowFirstPageBtn ? '←' : null}
+      <button className="btn" onClick={goPrev} disabled={isShowFirstPageBtn}>
+        {!isShowFirstPageBtn ? '←' : null}
       </button>
       <span className="pagination__page">{currentPage}</span>
-      <button className="btn" onClick={goNext}>
-        {isShowLastPageBtn ? '→' : null}
+      <button className="btn" onClick={goNext} disabled={isShowLastPageBtn}>
+        {!isShowLastPageBtn ? '→' : null}
       </button>
     </div>
   );
